f6f4a3b9f58072fc326b4e5afb5cc2ddbf16b6e5
SHONE\nenad <nenadkragovic@gmail.com>
2016-12-18 22:12:31 +0100

    pocetak

---
+++ Vibe/Scripts/Home/home.js
@@ -0,0 +1,19 @@
+﻿(function (ko) {
+    
+    var demoSongs = "[{\"Name\":\"Sorrow\",\"Artist\":\"Pink Floyd\"},{\"Name\":\"Numb\",\"Artist\":\"Linkin park\"}]";
+    function Home()
+    {
+        self.songs = ko.observableArray(JSON.parse(demoSongs));
+    }
+    function Player()
+    {
+        self.isPlaying = ko.observable(false);
+        self.play = function () {
+            self.isPlaying(true);
+        }
+        self.pause = function () {
+            self.isPlaying(false);
+        }
+    }
+    ko.applyBindings(new Home());
+})(ko);
