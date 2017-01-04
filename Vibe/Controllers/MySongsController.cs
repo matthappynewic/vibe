593b551c7f8991bb730752c04c5677884c75be7d
SHONE\nenad <nenadkragovic@gmail.com>
2017-01-04 16:46:56 +0100

    new pages added

---
+++ Vibe/Controllers/MySongsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vibe.Controllers
+{
+    public class MySongsController : Controller
+    {
+        // GET: MySongs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
